9a7d96ad3e2fa0af6df82c1ef68966d5dacd0e1d
Keenan Brock <keenan@thebrocks.net>
2017-11-06 17:38:39 -0500

    profile_methods to help wrap profiling

--- lib/cli_mini_profiler/helpers.rb
+++ lib/cli_mini_profiler/helpers.rb
@@ -4,6 +4,25 @@ def bookend(*args, &block)
   CliMiniProfiler.capture(*args, &block)
 end
 
+def profile_methods(*methods)
+  methods.flatten.each do |method_desc|
+    klass_name, mode, method_name = method_desc.split(/([.#])/)
+    klass = klass_name.safe_constantize
+
+    begin
+      if klass && mode == "."
+        ::Rack::MiniProfiler.profile_singleton_method(klass, method_name) { |a| method_desc }
+      elsif klass
+        ::Rack::MiniProfiler.profile_method(klass, method_name) { |a| method_desc }
+      else
+        puts "Can not find class: #{klass} (#{method_desc})"
+      end
+    rescue
+#        puts "Can not bind: #{method_desc}"
+    end
+  end
+end
+
 def bookendr(*args, &block)
   ActiveRecord::Base.transaction do
     CliMiniProfiler.capture(*args, &block)
